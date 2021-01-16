f0b86e3b95f8f226f4c0da48eed26f20255f291a
stefanoaragoni <stefanoaragoni13@gmail.com>
2021-01-16 21:29:59 +0000

    Stefano: Menu principal, cree opción 1,2,6

--- Main.java
+++ Main.java
@@ -1,11 +1,81 @@
 
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     boolean estado = false;
+    Scanner scan = new Scanner(System.in);
+    int loop = 1;
 
-    System.out.println("-Simulacion de Carro-\nQue desea hacer?");
-    System.out.println("1.Encender el radio \n2.Cambiar de AM a FM a AM \n3.Avanzar en el dial de las emisoras. Al llegar al final del dial inicia nuevamente.4.Permite guardar una emisora en uno de los 12 botones5.Permite seleccionar la emisora puesta en un botón6.Apagar el radio"); 
+    Radio radio = new Radio();
 
+    while(loop == 1){
+
+      System.out.println("-Simulacion de Carro-\nQue desea hacer?");
+      System.out.println("1.Encender el radio \n2.Cambiar de AM a FM a AM \n3.Avanzar en el dial de las emisoras. \n4.Guardar emisora \n5.Seleccionar emisora de un boton \n6.Apagar el radio"); 
+
+      System.out.println("Por favor ingrese el numero de la accion a realizar");
+
+      int opcion = 0;
+      while(opcion == 0){
+        try{
+          opcion = scan.nextInt();
+        }catch(Exception e){
+          System.out.println("Por favor ingresa un numero correcto.\n\n\n"); 
+          scan.next();
+          continue;
+        }
+        
+      }
+
+      switch(opcion){
+        case 1:
+          if(estado == true){
+            System.out.println("El radio ya esta encendido.\n\n\n"); 
+          }else{
+            estado = radio.encenderApagar();
+            System.out.println("Se ha encendido el radio.\n\n\n");
+          }
+          break;
+        case 2:
+          if(estado == true){
+            boolean tipo = radio.amFm();
+            if(tipo == false){
+              System.out.println("Ahora estas escuchando una emisora FM.\n\n\n"); 
+            }else{
+              System.out.println("Ahora estas escuchando una emisora AM.\n\n\n"); 
+            }
+            
+          }else{
+            System.out.println("El radio esta apagado. No se puede completar esta accion.\n\n\n"); 
+          }
+          break;
+        case 3:
+          if(estado == true){
+            double estacion = radio.avanzar();
+            System.out.println("Ahora estas escuchando: " + estacion+"\n\n\n");
+          }else{
+            System.out.println("El radio esta apagado. No se puede completar esta accion.\n\n\n");
+          }
+
+          break;
+        case 4:
+
+          break;
+        case 5:
+
+          break;
+        case 6:
+          if(estado == true){
+            estado = radio.encenderApagar();
+            System.out.println("Se ha apagado el radio.\n\n\n");
+          }else{
+            System.out.println("El radio ya esta apagado.\n\n\n"); 
+          }
+          break;
+      }
+
+    }
 
   }
 }
--- Radio.java
+++ Radio.java
@@ -4,10 +4,10 @@ public class Radio implements Interface{
 
   boolean estado = false;
   boolean emisora = false;
-  float estacion = 87.9;
+  double estacion = 87.9;
 
-  float[] AM = {530, 540, 550, 560, 570, 580, 590, 600, 610, 620, 630, 640}; 
-  float[] FM = {87.9,88.1,88.3,88.5,88.7,88.9,89.1,89.3,89.5,89.7,89.9, 90.1}; 
+  double[] AM = {530, 540, 550, 560, 570, 580, 590, 600, 610, 620, 630, 640}; 
+  double[] FM = {87.9,88.1,88.3,88.5,88.7,88.9,89.1,89.3,89.5,89.7,89.9, 90.1}; 
 
 
   public boolean encenderApagar(){
@@ -22,8 +22,10 @@ public class Radio implements Interface{
   public boolean amFm(){
     if(emisora == false){
       emisora = true;
+      estacion = 530;
     }else{
       emisora = false;
+      estacion = 87.9;
     }
     return emisora;
 
@@ -32,7 +34,7 @@ public class Radio implements Interface{
   public String guardar(int btn){
     String message = "";
     if(emisora == false){ //FM
-      FM[btn] = estacion;
+      FM[btn-1] = estacion;
 
       String boton = String.valueOf(btn);
       String emisoraString = String.valueOf(emisora);
@@ -41,7 +43,7 @@ public class Radio implements Interface{
 
 
     }else{ //AM
-      AM[btn] = estacion;
+      AM[btn-1] = estacion;
 
       String boton = String.valueOf(btn);
       String emisoraString = String.valueOf(emisora);
@@ -49,20 +51,40 @@ public class Radio implements Interface{
       message = "El boton "+boton+"ha guardado la emisora "+emisoraString+" AM.";
 
     }
+    return message;
 
   }
 
   public String seleccionar(int btn){
+    String message = "";
+    if (emisora == false){
+      estacion = FM[btn-1];
 
+      message = "Ha seleccioned el boton " + String.valueOf(btn) + " con la emisora " + String.valueOf(estacion) + " FM."; 
+    }else{
+      estacion = AM[btn-1];
+
+      message = "Ha seleccioned el boton " + String.valueOf(btn) + " con la emisora " + String.valueOf(estacion) + " AM."; 
+
+    }
+  
+    return message;
 
   }
 
   public double avanzar(){
     if (emisora == false){
-      //estacion = 
+      estacion += 0.2;
+      if (estacion > 107.9){
+        estacion = 87.9;
+      }
+    } else{
+      estacion += 10;
+      if (estacion > 1610){
+        estacion = 530;
+      }
     }
-
-
+    return estacion;
   }
 
 
